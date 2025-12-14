346c8131606267b4092ab0299cfdd7ff275f701e
Agape <theaman249@gmail.com>
2025-12-14 22:45:20 +0200

    initial commit on develop

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
